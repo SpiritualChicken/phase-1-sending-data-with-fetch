2f8873b0fb451191881ab8627e9cb1a6c958f6cb
Charlie <cnewlinds@gmail.com>
2023-09-02 19:55:22 +1000

    sumbit data function

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your code here
+const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify({
+      name: "Steve",
+      email: "steve@steve.com"
+    }),
+};
+
+
+function submitData() {
+    return fetch("http://localhost:3000/users", configurationObject) 
+         .then(resp => resp.json())
+         .then(data => document.querySelector("body").append(`<p>${data.id}</p>`) )
+        .catch((error) => document.querySelector("body").innerHTML += `<div id="error">${error.message}</div>`) 
+
+}
